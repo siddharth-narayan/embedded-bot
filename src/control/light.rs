c5a94db6e42cea8f80bdb0d43d7e20b64cc5d5e4
Siddharth Narayan <26383051+siddharth-narayan@users.noreply.github.com>
2026-03-05 07:21:16 +0000

    Add extra light utils, better test

--- src/control/light.rs
+++ src/control/light.rs
@@ -4,7 +4,7 @@ use i2cdev::linux::LinuxI2CError;
 
 use crate::control::{Register, Robot, ControlError};
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct LightColor {
     r: u8,
     g: u8,
@@ -28,6 +28,10 @@ impl Robot {
         color: LightColor,
     ) -> ControlError<LinuxI2CError> {
 
+        if light > 8 {
+            return Ok(());
+        }
+
         self.write_block_data(
             Register::WQ2812BrightnessAlone as u8,
             &[light, color.r, color.g, color.b],
@@ -36,10 +40,27 @@ impl Robot {
         Ok(())
     }
 
+    pub fn set_all_lights(
+        &mut self,
+        color: LightColor
+    ) -> ControlError<LinuxI2CError> {
+        for l in 0u8..9 {
+            self.set_light(l, color)?;
+        }
+
+        Ok(())
+    }
+
     pub(super) fn test_lights(&mut self) -> ControlError<LinuxI2CError> {
-        for light in 0u8..8 {
-            self.set_light(light, LightColor::new(255, 0, 0))?;
-            sleep(Duration::from_millis(100))
+        for _ in 0..3 {        
+            self.set_all_lights(LightColor::new(255, 0, 0))?;
+            sleep(Duration::from_millis(1000));
+
+            self.set_all_lights(LightColor::new(0, 255, 0))?;
+            sleep(Duration::from_millis(1000));
+
+            self.set_all_lights(LightColor::new(0, 0, 255))?;
+            sleep(Duration::from_millis(1000));
         }
 
         Ok(())
